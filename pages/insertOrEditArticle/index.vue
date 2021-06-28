28810ef61d763b465d38a163fdd1bed74ab897b0
xy hua <1095159835@qq.com>
2021-06-28 11:54:22 +0800

    增加添加文章

--- pages/insertOrEditArticle/index.vue
+++ pages/insertOrEditArticle/index.vue
@@ -4,21 +4,38 @@
     <div class="row">
 
       <div class="col-md-10 offset-md-1 col-xs-12">
-        <form>
+        <form >
           <fieldset>
             <fieldset class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Article Title">
+                <input type="text" 
+                       class="form-control form-control-lg" 
+                       required
+                       v-model="article.title"
+                       placeholder="Article Title">
             </fieldset>
             <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="What's this article about?">
+                <input type="text" 
+                       class="form-control" 
+                       v-model="article.description"
+                       placeholder="What's this article about?">
             </fieldset>
             <fieldset class="form-group">
-                <textarea class="form-control" rows="8" placeholder="Write your article (in markdown)"></textarea>
+                <textarea class="form-control" 
+                          rows="8" 
+                          required
+                          v-model="article.body"
+                          placeholder="Write your article (in markdown)"></textarea>
             </fieldset>
             <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter tags"><div class="tag-list"></div>
+                <input type="text" 
+                       class="form-control" 
+                       v-model="article.tagList"
+                       placeholder="Enter tags separate with ,"><div class="tag-list"></div>
             </fieldset>
-            <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+            <button class="btn btn-lg pull-xs-right btn-primary" 
+                    type="button"
+                    @click="handler"
+                    >
                 Publish Article
             </button>
           </fieldset>
@@ -34,9 +51,34 @@
 </template>
 
 <script>
+import { createArticle } from '@/api/article'
 export default {
     middleware: 'auth', 
-    name: 'insertOrEditArticlePage'
+    name: 'insertOrEditArticlePage',
+    data: function () {
+      return {
+        article: {
+          title: '',
+          description: '',
+          body: '',
+          tagList: ''
+        }
+      }
+    },
+    methods: {
+      async handler () {
+        let data = this.article;
+        data.tagList = typeof this.article.tagList === 'string'?this.article.tagList.split(','):data.tagList;
+        try{
+          await createArticle(data);
+          this.$router.push({
+            name:'home'
+          })
+        }catch(err){
+          alert(err)
+        }
+      }
+    }
 }
 </script>
 
